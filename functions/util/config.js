658225e2882964063d5050a3ee77ba9f1ee7300b
Atwinenickson <atwiinenicxon@gmail.com>
2020-04-29 21:30:10 +0300

    Added firebase app with add users and get posts function

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyBeksl4nQV8sFJn4PNp1nw4-SStzsYshqI",
+  authDomain: "socialmedia-b1a5e.firebaseapp.com",
+  databaseURL: "https://socialmedia-b1a5e.firebaseio.com",
+  projectId: "socialmedia-b1a5e",
+  storageBucket: "socialmedia-b1a5e.appspot.com",
+  messagingSenderId: "486894182527",
+  appId: "1:486894182527:web:69cf75b5fa233c04281f4a",
+  measurementId: "G-4CXKV0G09F",
+};
